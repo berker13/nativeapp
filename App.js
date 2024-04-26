dd3626f49225e65c94390814f9b1a17467104ee4
Berker <vojta.janota13@gmail.com>
2024-04-26 10:25:44 +0200

    fff

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View, ScrollView } from 'react-native';
+import MyStack from './components/pages/router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>fortnite mobile</Text>
-      <StatusBar style="auto" />
-    </View>
+      <MyStack/>
   );
 }
 
 const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  scrollView: {
+    marginHorizontal: 20,
   },
 });
