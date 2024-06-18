f4f686b5e7e5480ba1b428d9f78428445699e8d4
berker13 <148329668+berker13@users.noreply.github.com>
2024-06-18 21:42:42 +0200

    imgpicker

--- App.js
+++ App.js
@@ -1,31 +1,59 @@
-import { StyleSheet, Button, View, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Button, View, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 import MyTabs from './components/bottom_navigation_bar';
 
 export default function App() {
-  return (
-      <View style ={styles.container}>
-        <MyTabs/>
-      </View>
-  );
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const pickImage = async () => {
+        let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if (permissionResult.granted === false) {
+            alert("Permission to access camera roll is required!");
+            return;
+        }
+
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        if (!result.cancelled) {
+            setSelectedImage(result.uri);
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <MyTabs />
+            <Button title="Pick an image from camera roll" onPress={pickImage} />
+            {selectedImage && <Image source={{ uri: selectedImage }} style={styles.image} />}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 4,
-    elevation: 3,
-    backgroundColor: 'black',
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  scrollView: {
-    marginHorizontal: 20,
-  },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: 'black',
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image: {
+        width: 200,
+        height: 200,
+        marginTop: 20,
+    },
 });
